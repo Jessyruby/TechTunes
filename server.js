af3e252d91291274b8fa939d9e218f13106eb043
Jessica Cardoso <jessicacp2004@gmail.com>
2024-08-16 21:21:35 -0300

    Permitir todas as origens
    Esse erro que você está enfrentando ocorre por causa de uma política de segurança dos navegadores chamada CORS (Cross-Origin Resource Sharing). Ela impede que seu frontend (no http://127.0.0.1:5500) faça requisições para um backend em um domínio diferente (https://techtunes.onrender.com) sem a devida autorização

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ require('./config/db.js')
 const app = require('express')()
 const PORT = 3000
 
+const cors = require('cors');
+
+// Permitir todas as origens (cuidado em produção)
+app.use(cors());
+
 const UserRoutes = require('./api/User.js')
 
 const bodyParser = require('express').json
